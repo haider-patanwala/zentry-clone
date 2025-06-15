9a4de3ee82b4f6d66626376a3716676c2425f824
Haider Patanwala <haiderpatanwala@gmail.com>
2025-06-15 22:46:54 +0530

    feat: add About component to App layout and remove unused section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function App() {
 	return (
 		<main className='relative min-h-dvh w-dvw overflow-x-hidden'>
 			<Hero />
-			<section className='z-0 min-h-screen bg-red-50'></section>
+			<About />
+			{/* <section className='z-0 min-h-screen bg-red-50'></section> */}
 		</main>
 	);
 }
