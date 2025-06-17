22f1fb66f8ae42ddbd53c588754b0cae7ba2139a
Haider Patanwala <haiderpatanwala@gmail.com>
2025-06-18 00:38:59 +0530

    feat: add NavBar component to App layout for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
+import NavBar from "./components/NavBar";
 
 export default function App() {
 	return (
 		<main className='relative min-h-dvh w-dvw overflow-x-hidden'>
+			<NavBar />
 			<Hero />
 			<About />
-			{/* <section className='z-0 min-h-screen bg-red-50'></section> */}
 		</main>
 	);
 }
