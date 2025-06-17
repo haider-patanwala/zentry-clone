39c881f0d793902d36b3d36193d710d9069c6933
Haider Patanwala <haiderpatanwala@gmail.com>
2025-06-18 01:17:59 +0530

    refactor: remove background color from main layout for cleaner design

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar";
 
 export default function App() {
 	return (
-		<main className='relative bg-slate-800 min-h-dvh w-dvw overflow-x-hidden'>
+		<main className='relative min-h-dvh w-dvw overflow-x-hidden'>
 			<NavBar />
 			<Hero />
 			<About />
